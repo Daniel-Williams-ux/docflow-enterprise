a75219e0310847f5ca8e0bd1c5418514ef8e98d6
Daniel <williamsoligodaniel@gmail.com>
2026-05-24 20:43:17 +0100

    connect processing service to processing repository

--- src/modules/processing/processing.service.ts
+++ src/modules/processing/processing.service.ts
@@ -1,6 +1,12 @@
+import { ProcessingRepository } from './processing.repository';
+
 export class ProcessingService {
     async createProcessingJob() {
         // create processing job record
+        const processingRepository = new ProcessingRepository();
+        const processingJob = await processingRepository.createProcessingJob();
         // enqueue processing job for background work
+
+        return processingJob;
     }
 }
